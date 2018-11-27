4407c0b06364d1921a4b188b140f23b51a88cec2
Matus Zamborsky <zamborsky@gmail.com>
2018-11-27 09:38:56 +0100

    fixed build

--- src/index.ts
+++ src/index.ts
@@ -87,7 +87,6 @@ export function withdrawOng({ client, account, password, ...rest }: TransferOpti
 export { CompilerType, CompilerOutput } from './compiler';
 export { loadContract, loadCompiledContract } from './common/utils';
 export { loadWallet, createWallet, createAccount } from './wallet';
-export { compileCli, deployCli, invokeCli, invokeFileCli } from './cli/cli';
 export { hex2num, reverseBuffer } from './common/utils';
 export { RpcClient } from './network/rpcClient';
 export { buildInvokePayload } from './invoker';
